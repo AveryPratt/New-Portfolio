03b6be81065c4f0a58769cbf63e69ee7d264b21a
Avery Pratt <apratt91@gmail.com>
2017-12-05 23:06:32 -0800

    still trying to get around bug with THREE objects

--- js/index.js
+++ js/index.js
@@ -6,5 +6,20 @@ window.onload = function(){
 
     var scene = THREE.Scene();
     var camera = THREE.PerspectiveCamera();
-    var renderer = THREE.Renderer();
+    var renderer = THREE.WebGLRenderer();
+
+    var geometry = new THREE.BoxGeometry(10,10,10);
+    var mesh = new THREE.Mesh(geometry);
+
+    scene.add(mesh);
+    camera.position.z = 100;
+
+    var loop = function(){
+        requestAnimationFrame(loop);
+
+        renderer.render(scene, camera);
+        ctx.render(scene, camera);
+    }
+
+    loop();
 };
