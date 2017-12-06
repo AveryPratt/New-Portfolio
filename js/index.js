415b36b7438bbb299d8bf5fafaf47cf9846768de
Avery Pratt <apratt91@gmail.com>
2017-12-06 00:38:38 -0800

    finally got a good scene

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
-window.onload = function(){
+// window.onload = function(){
+    const deg = 180 / Math.PI;
     var canvas = document.getElementById('background-canvas');
+
     var renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
-    renderer.setClearColor(0x00ff00);
+    renderer.setClearColor(0xddffff);
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     
@@ -11,30 +13,29 @@ window.onload = function(){
     var scene = new THREE.Scene();
 
     var light = new THREE.AmbientLight(0xffffff, .5);
-    var light1 = new THREE.PointLight(0xffffff, .5);
-    light1.position.x = 50;
-    light1.position.z = 50;
-    light1.position.y = 100;
+    var dirLight = new THREE.DirectionalLight(0xffffff, .5);
+    dirLight.position.y = 100;
+    dirLight.position.z = 10;
+    dirLight.position.x = 10;
+    scene.add(light);
+    scene.add(dirLight);
+    
 
     var geometry = new THREE.BoxGeometry(10,10,10);
     var material = new THREE.MeshLambertMaterial(0xbbbbbb);
     var mesh = new THREE.Mesh(geometry, material);
-    
-    scene.add(light);
-    scene.add(light1);
     scene.add(mesh);
+    camera.position.y = 50;
     camera.position.z = 100;
+    camera.rotateX(-30 / deg);
+    mesh.rotateY(45 / deg);
 
     var loop = function(){
         requestAnimationFrame(loop);
-
-        mesh.rotateX(.008);
-        mesh.rotateY(.01);
-        mesh.rotateZ(.012);
 
         renderer.render(scene, camera);
     }
 
     loop();
 
-};
+// };
