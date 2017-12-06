396f2d482cfafc9ad62ee324c16c9d70880d275c
Avery Pratt <apratt91@gmail.com>
2017-12-06 01:46:52 -0800

    made a magic carpet

--- js/index.js
+++ js/index.js
@@ -14,24 +14,33 @@
 
     var light = new THREE.AmbientLight(0xffffff, .5);
     var dirLight = new THREE.DirectionalLight(0xffffff, .5);
-    dirLight.position.y = 100;
+    dirLight.position.y = 10;
     dirLight.position.z = 10;
-    dirLight.position.x = 10;
+    dirLight.position.x = 100;
     scene.add(light);
     scene.add(dirLight);
     
-
-    var geometry = new THREE.BoxGeometry(10,10,10);
-    var material = new THREE.MeshLambertMaterial(0xbbbbbb);
+    var geometry = new THREE.PlaneGeometry(100, 100, 10, 10);
+    var delta = 0;
+    var material = new THREE.MeshLambertMaterial(0xff0000);
     var mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
     camera.position.y = 50;
-    camera.position.z = 100;
+    camera.position.z = 120;
     camera.rotateX(-30 / deg);
-    mesh.rotateY(45 / deg);
+    mesh.rotateX(-90 / deg);
 
     var loop = function(){
         requestAnimationFrame(loop);
+
+        delta += .015;
+        for (var i = 0; i <= 10; i++){
+            for (var j = 0; j <= 10; j++){
+                var idx = i * 11 + j;
+                geometry.vertices[idx].z = Math.sin(delta + i / 5 - j / 10) * 10 - 5;
+            }
+        }
+        geometry.verticesNeedUpdate = true;
 
         renderer.render(scene, camera);
     }
